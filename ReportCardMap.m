98282f8985b63eba580b79622aeb2f6216dacf56
Guolin Yao <yaoguolin0523@gmail.com>
2021-04-15 12:57:55 -0400

    Update ReportCardMap.m

--- ReportCardMap.m
+++ ReportCardMap.m
@@ -1,5 +1,5 @@
 clear;
-
+% Change the directory when needed
 dir = 'C:\Users\gyao\Google Drive\ZhangLabData08222019\SAM\SAM_Model\';
 dir_A = 'C:\Users\gyao\Google Drive\SAM_CMT\Inputs\';
 dir_gis = 'C:\Users\gyao\Google Drive\ArcGIS\';
@@ -37,32 +37,32 @@ end
 
 
 
-vec = [      100;       84;       67;       50;       33;       17;        0];
-hex = ['#00ff00';'#7bff00';'#ccff00';'#ffff00';'#ffb300';'#ff8000';'#ff0000'];
-raw = sscanf(hex','#%2x%2x%2x',[3,size(hex,1)]).' / 255;
-N = 100;
-%N = size(get(gcf,'colormap'),1) % size of the current colormap
-map = interp1(vec,raw,linspace(0,100,N),'pchip');
-mapn = nan(101,3);
-mapn(1,:) = [1 1 1];
-mapn(2:101,:) = map;
+% vec = [      100;       84;       67;       50;       33;       17;        0];
+% hex = ['#00ff00';'#7bff00';'#ccff00';'#ffff00';'#ffb300';'#ff8000';'#ff0000'];
+% raw = sscanf(hex','#%2x%2x%2x',[3,size(hex,1)]).' / 255;
+% N = 100;
+% %N = size(get(gcf,'colormap'),1) % size of the current colormap
+% map = interp1(vec,raw,linspace(0,100,N),'pchip');
+% mapn = nan(101,3);
+% mapn(1,:) = [1 1 1];
+% mapn(2:101,:) = map;
 
-colormap(mapn)
-S = shaperead(shapefile);
-figure('units','normalized','outerposition',[0 0 1 1],'Color','White');
-set(gca,'xcolor','none','ycolor','none')
+% colormap(mapn)
+% S = shaperead(shapefile);
+% figure('units','normalized','outerposition',[0 0 1 1],'Color','White');
+% set(gca,'xcolor','none','ycolor','none')
 
-title('Social','FontSize', 30)
-faceColors = makesymbolspec('Polygon', ...
-  {'Social',[-1 100],'FaceColor',mapn,'EdgeColor', [0.25 0.25 0.25]});
-mapshow(S,'SymbolSpec', faceColors);
+% title('Social','FontSize', 30)
+% faceColors = makesymbolspec('Polygon', ...
+%   {'Social',[-1 100],'FaceColor',mapn,'EdgeColor', [0.25 0.25 0.25]});
+% mapshow(S,'SymbolSpec', faceColors);
 
-colormap(map)
-c = colorbar;
-c.TickLabels = [0 10 20 30 40 50 60 70 80 90 100];
-c.FontSize = 16;
+% colormap(map)
+% c = colorbar;
+% c.TickLabels = [0 10 20 30 40 50 60 70 80 90 100];
+% c.FontSize = 16;
 
-print([dir_fig 'Economic'],'-dpng','-r200')
+% print([dir_fig 'Economic'],'-dpng','-r200')
 
 
 
